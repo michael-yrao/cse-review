995570f0c33d04e32dadd0f345b7d1c79f2c5464
michael-yrao <yao.rao@alumni.stonybrook.edu>
2023-01-06 16:53:22 -0500

    Added problem 1299 to learn about range in py

---
+++ dataStructureAlgorithm/leetcode/python/1299_ReplaceElementsWithGreatestElementOnRight.py
@@ -0,0 +1,31 @@
+class Solution:
+    def replaceElements(self, arr: List[int]) -> List[int]:
+        # [17,18,5,4,6,1]
+        # [18,6,6,6,6,-1] <- construct this array from right side
+        # curMax = -1
+        
+        rightList = [-1] * len(arr)
+        rightMax = -1
+
+        # range(start,stop,step)
+        for index in range(len(arr) - 1, -1, -1):
+            currentMax = max(rightMax,arr[index])
+            rightList[index] = rightMax
+            rightMax = currentMax
+        
+        return rightList
+
+    def replaceElementsSingleArray(self, arr: List[int]) -> List[int]:
+        # [17,18,5,4,6,1]
+        # [18,6,6,6,6,-1] <- construct this array from right side
+        # curMax = -1
+        
+        rightMax = -1
+
+        # range(start,stop,step)
+        for index in range(len(arr) - 1, -1, -1):
+            currentMax = max(rightMax,arr[index])
+            arr[index] = rightMax
+            rightMax = currentMax
+        
+        return arr
