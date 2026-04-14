2d4a34125bfceb3a9eb83e625918e39c854e0a8f
michael-yrao <yao.michael.rao@gmail.com>
2026-04-13 22:43:21 -0400

    Added LC 33

--- dataStructureAlgorithm/leetcode/2026_python_doodles/playground.py
+++ dataStructureAlgorithm/leetcode/2026_python_doodles/playground.py
@@ -379,3 +379,44 @@ class Playground:
                 # equal means we need to keep l in place to perform replacements
                 r+=1
             return l
+    class SearchInRotatedSortedArray:
+        def search(self, nums: List[int], target: int) -> int:
+            # if we are looking for logn time, we can't go through the array to find k
+            # we should binary search to find k
+            # then binary search on the 2 halves
+            # k is smallest value, thus we do binary search on smallest value
+            # since we are not looking for an exact value, we should do while l < r instead of while l <= r
+            # Want exact target index? l <= r
+            # Want first/last occurrence or smallest pivot? l < r
+            # If one branch keeps mid as possible answer, prefer l < r
+            # If both branches exclude mid, use l <= r
+
+            l, r = 0, len(nums) - 1
+
+            while l < r:
+                mid = (l+r) // 2
+                if nums[mid] > nums[r]:
+                    l = mid + 1
+                else:
+                    r = mid
+            
+            k = l
+
+            # binary search on both halves
+
+            def binarySearch(l,r) -> int:
+                while l<=r:
+                    mid = (l+r)//2
+                    if nums[mid] == target:
+                        return mid
+                    elif nums[mid] > target:
+                        r = mid - 1
+                    else:
+                        l = mid + 1
+                return -1
+            
+            result = binarySearch(0,k-1)
+            if result == -1:
+                result = binarySearch(k,len(nums)-1)
+            
+            return result
